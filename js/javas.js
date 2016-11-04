8b91588d3db1cbea118ef0061b5b045f0287aa91
kellyurribarr <kellyurribarri@gmail.com>
2016-11-04 14:07:53 -0300

    agregar corazon y basurero

--- js/javas.js
+++ js/javas.js
@@ -7,21 +7,30 @@ function agregarLista() {
 	var crearSpan = document.createElement('span');
 	var crearInput = document.createElement('input');
 	var crearA = document.createElement('a');
+	var crearBasurero = document.createElement('a');
 	var crearI = document.createElement('i');
 	var crearCorazon = document.createElement('i');
+
+
 	var crearPadre = document.getElementById('contenedor-agregar');
 
 	//atributos --- dandole valor
 	crearInput.setAttribute('type','checkbox');
+	crearCorazon.setAttribute('class','fa fa-heart');
+	crearI.setAttribute('class','fa fa-trash');
 
 	var creartexto = document.createTextNode(guardarContenido);
 	//asignacion de padres/hijos
-	
+	crearA.appendChild(crearCorazon);
+	crearBasurero.appendChild(crearI);
 	
 	//crearLabel.appendChild(crearInput);
 	crearSpan.appendChild(creartexto);
 	crearDiv.appendChild(crearInput);
 	crearDiv.appendChild(crearSpan);
-	
+	crearDiv.appendChild(crearA);
+	//crearDiv.appendChild(crearCorazon);
+	crearDiv.appendChild(crearBasurero);
+	//crearDiv.appendChild(crearI);
 	crearPadre.appendChild(crearDiv);
 }

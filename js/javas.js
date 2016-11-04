6268389fedca4d48f0ded8c1450fde0a4b180b2d
kellyurribarr <kellyurribarri@gmail.com>
2016-11-04 13:33:35 -0300

    funcion agregar funcionando

--- js/javas.js
+++ js/javas.js
@@ -3,13 +3,25 @@ function agregarLista() {
 	var guardarContenido = document.getElementById('caja-agregar').value;
 	//crear un div
 	var crearDiv = document.createElement('div');
-	var crearLabel = document.createElement('label');
+	//var crearLabel = document.createElement('label');
+	var crearSpan = document.createElement('span');
 	var crearInput = document.createElement('input');
 	var crearA = document.createElement('a');
 	var crearI = document.createElement('i');
+	var crearCorazon = document.createElement('i');
+	var crearPadre = document.getElementById('contenedor-agregar');
 
 	//atributos --- dandole valor
-	crearInput.setAtribute('type', 'checkbox');
+	crearInput.setAttribute('type','checkbox');
 
 	var creartexto = document.createTextNode(guardarContenido);
+	//asignacion de padres/hijos
+	
+	
+	//crearLabel.appendChild(crearInput);
+	crearSpan.appendChild(creartexto);
+	crearDiv.appendChild(crearInput);
+	crearDiv.appendChild(crearSpan);
+	
+	crearPadre.appendChild(crearDiv);
 }

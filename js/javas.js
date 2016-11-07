781f8e16e486b346f84c66ffc80c1857b14e040f
kellyurribarr <kellyurribarri@gmail.com>
2016-11-07 09:33:24 -0300

    agregue funcion tachar y cambiar color

--- js/javas.js
+++ js/javas.js
@@ -1,3 +1,4 @@
+var idLista = 0;
 function agregarLista() {
 	//guarda contenido de caja-agregar html
 	var guardarContenido = document.getElementById('caja-agregar').value;
@@ -10,14 +11,21 @@ function agregarLista() {
 	var crearBasurero = document.createElement('a');
 	var crearI = document.createElement('i');
 	var crearCorazon = document.createElement('i');
-
-
 	var crearPadre = document.getElementById('contenedor-agregar');
 
 	//atributos --- dandole valor
 	crearInput.setAttribute('type','checkbox');
+	crearInput.setAttribute("class", "tachar");
 	crearCorazon.setAttribute('class','fa fa-heart');
 	crearI.setAttribute('class','fa fa-trash');
+	crearDiv.setAttribute("id", idLista);
+	crearBasurero.setAttribute("onclick", "borrar("+ idLista+ ")");
+
+	//agregar clases
+	crearInput.classList.add("col-xs-1");
+	crearSpan.classList.add("col-xs-9");
+	crearCorazon.classList.add("col-xs-1");
+	crearBasurero.classList.add("col-xs-1");
 
 	var creartexto = document.createTextNode(guardarContenido);
 	//asignacion de padres/hijos
@@ -33,4 +41,19 @@ function agregarLista() {
 	crearDiv.appendChild(crearBasurero);
 	//crearDiv.appendChild(crearI);
 	crearPadre.appendChild(crearDiv);
+
+	//funcion tachar
+	crearInput.addEventListener("click", function(){
+		crearSpan.classList.toggle("tachar");
+	});
+
+	//funcion color rojo
+	crearA.addEventListener("click", function(){
+		crearCorazon.classList.toggle("rojo");
+	});
 }
+
+function borrar(idLista){
+ var element = document.getElementById(idLista);
+ element.parentNode.removeChild(element);
+ }
